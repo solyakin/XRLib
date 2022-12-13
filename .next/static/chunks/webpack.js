9829c03f10d11fe09b2a0b6ff959ba391f87019c
solomon Akinlade <48894861+solyakin@users.noreply.github.com>
2022-12-13 11:00:21 +0100

    adding seo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d82b59b62fbb820e"; }
+/******/ 		__webpack_require__.h = function() { return "920000cfdb947ebc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/RecentPodcast.jsx
+++ components/RecentPodcast.jsx
@@ -1,30 +1,37 @@
 import Link from 'next/link'
 import React from 'react'
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 import styles from '../styles/Recent.module.css'
 
-const RecentPodcast = () => {
+const RecentPodcast = ({podcasts}) => {
+
   return (
     <div className={styles.recent}>
         <img src="/grid bg.svg" alt="" className={styles.grid_bg} />
         <div className={styles.recent_wrapper}>
             <h3>Recent Podcasts</h3>
             <div className={styles.dflex}>
-                <div className={styles.item}>
-                    <img src="/profile_3.svg" alt="" className={styles.post_img} />
-                    <div className={styles.content}>
-                        <h5>How Augmented Reality is Revolutionalizing Education - Ludenso</h5>
-                        <p>Do you really need to waste your time learning Web 3 if it is just a failed idea? Intro For the majority of people, Web 3.0 sounds like one more buzzword used to promote crypto scams or get a piece of hype.</p>
-                        <img src="/Group 70.svg" alt="" />
-                    </div>
-                </div>
-                <div className={styles.item}>
-                    <img src="/profile_3.svg" alt="" className={styles.post_img} />
-                    <div className={styles.content}>
-                        <h5>How Augmented Reality is Revolutionalizing Education - Ludenso</h5>
-                        <p>Do you really need to waste your time learning Web 3 if it is just a failed idea? Intro For the majority of people, Web 3.0 sounds like one more buzzword used to promote crypto scams or get a piece of hype.</p>
-                        <img src="/Group 70.svg" alt="" />
-                    </div>
-                </div>
+                {
+                    podcasts && podcasts.slice(0, 2).map(({title, contentSnippet, enclosure}) => {
+                        return(
+                            <div className={styles.item}>
+                                <img src="/profile_3.svg" alt="" className={styles.post_img} />
+                                <div className={styles.content}>
+                                    <h5>{title}</h5>
+                                    <p>{`${contentSnippet.substr(0, 220)} ...`}</p>
+                                    <div className={styles.audio}>
+                                            <AudioPlayer
+                                            src={enclosure.url}
+                                            onPlay={e => console.log("onPlay")}
+                                            style={{borderRadius : "45px", background : "rgba(255, 255, 255, 0.1)", border : "1px solid rgba(255, 255, 255, 0.1)"}}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
             <div className={styles.button}>
                 <button>
