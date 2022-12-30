22bdffa74ab577c6ad0ac86fee53f4d3ee866e16
solomon Akinlade <48894861+solyakin@users.noreply.github.com>
2022-12-30 18:59:45 +0100

    new changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5e4cb497eeff2542"; }
+/******/ 		__webpack_require__.h = function() { return "a9bf34836d714998"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
